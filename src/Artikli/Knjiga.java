63750c7e2e58a3564a416176284053e1b6a0ade4
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-03-15 22:25:00 +0100

    added equals, hashCode and toString methods

--- src/Artikli/Knjiga.java
+++ src/Artikli/Knjiga.java
@@ -29,4 +29,46 @@ public class Knjiga {
 	public void setKolicina(int kolicina) {
 		Kolicina = kolicina;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Kolicina;
+		result = prime * result + ((Naziv == null) ? 0 : Naziv.hashCode());
+		result = prime * result + ((Opis == null) ? 0 : Opis.hashCode());
+		result = prime * result + Sifra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (Kolicina != other.Kolicina)
+			return false;
+		if (Naziv == null) {
+			if (other.Naziv != null)
+				return false;
+		} else if (!Naziv.equals(other.Naziv))
+			return false;
+		if (Opis == null) {
+			if (other.Opis != null)
+				return false;
+		} else if (!Opis.equals(other.Opis))
+			return false;
+		if (Sifra != other.Sifra)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [Naziv=" + Naziv + ", Sifra=" + Sifra + ", Opis=" + Opis + ", Kolicina=" + Kolicina + "]";
+	}
+	
+	
 }
