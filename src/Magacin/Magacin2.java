793b012a6ef4ca3c970369cca66c765f2e4605b0
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-03-15 22:55:55 +0100

    added implementation of MagacinInterfejs

---
+++ src/Magacin/Magacin2.java
@@ -0,0 +1,64 @@
+package Magacin;
+
+import Artikli.KancelarijskiMaterijal;
+import Artikli.Knjiga;
+import Artikli.KucnaHemija;
+import Interfaces.MagacinInterfejs;
+
+public class Magacin2 implements MagacinInterfejs {
+
+	@Override
+	public void DodajKnjigu(Knjiga k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DodajKancMaterija(KancelarijskiMaterijal km) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DodajKucnuHemiju(KucnaHemija kh) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void IzbaciKnjigu(Knjiga k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void IzbaciKancMaterija(KancelarijskiMaterijal km) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void IzbaciKucnuHemiju(KucnaHemija kh) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Knjiga NadjiKnjigu(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public KancelarijskiMaterijal NadjiKancMaterija(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public KucnaHemija NadjiKucnuHemiju(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
